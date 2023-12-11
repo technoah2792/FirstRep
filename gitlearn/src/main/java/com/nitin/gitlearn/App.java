26aefca819b89e1cc064ffcec96039210525a1e6
technoah2792 <nnigam.2792@gmail.com>
2023-12-11 22:53:08 +0800

    first commit | Eclipse project

---
+++ gitlearn/src/main/java/com/nitin/gitlearn/App.java
@@ -0,0 +1,13 @@
+package com.nitin.gitlearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
