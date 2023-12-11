7eefe5a8a66e6c578446b79dce583d6791a5edcb
technoah2792 <nnigam.2792@gmail.com>
2023-12-11 23:39:23 +0800

    commit the substract method

--- gitlearn/src/main/java/com/nitin/gitlearn/App.java
+++ gitlearn/src/main/java/com/nitin/gitlearn/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        System.out.prinln("substraction: "+ substract(10, 5));
+    }
+    
+    public static int substract(int a, int b) {
+    	return (a - b);
     }
 }
