8b0a872f31804a0dbf2ea78d620436e0cc3e74dd
technoah2792 <nnigam.2792@gmail.com>
2023-12-11 23:29:58 +0800

    Added new method to sum

--- gitlearn/src/main/java/com/nitin/gitlearn/App.java
+++ gitlearn/src/main/java/com/nitin/gitlearn/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(sum(10,30));
+    }
+    
+    public static int sum (int a, int b) {
+    	return a+b;
     }
 }
