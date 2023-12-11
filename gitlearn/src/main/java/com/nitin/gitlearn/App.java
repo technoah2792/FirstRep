df2717f94b1c521163443f6b79623cad0db44431
technoah2792 <nnigam.2792@gmail.com>
2023-12-11 23:46:02 +0800

    Adding multiply method

--- gitlearn/src/main/java/com/nitin/gitlearn/App.java
+++ gitlearn/src/main/java/com/nitin/gitlearn/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println(sum(10,30));
         System.out.prinln("substraction: "+ substract(10, 5));
+        System.out.prinln("multiply: "+ multiply(7, 8));
     }
     
     public static int sum (int a, int b) {
@@ -19,5 +20,9 @@ public class App
     
     public static int substract(int a, int b) {
     	return (a - b);
+    }
+
+    public static int multiply(int a, int b) {
+        return (a * b);
     }
 }
