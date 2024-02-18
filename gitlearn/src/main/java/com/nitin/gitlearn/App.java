32b7ba7087a8dc929591db709d93904567f7432c
technoah2792 <nnigam.2792@gmail.com>
2024-02-18 16:17:31 +0800

    change in dev

--- gitlearn/src/main/java/com/nitin/gitlearn/App.java
+++ gitlearn/src/main/java/com/nitin/gitlearn/App.java
@@ -26,6 +26,11 @@ public class App
     public static int multiply(int a, int b) {
         return (a * b);
     }
+
+
+    public static int newcalculation(int a, int b) {
+        return (a * b * b * b)/(a * a);
+    }
     
     public static String printDoc() {
     	return "printDoc method";
